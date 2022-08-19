cd6c59105afc7fad1f006d74acbbfd5d6f87133b
Teerathep <popdopy4@gmail.com>
2022-08-19 22:19:02 +0700

    update

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "booking.cdchmht6aepu.ap-southeast-1.rds.amazonaws.com";
+$username = "admin";
+$password = "admin1234";
 $dbname = "dbbooking";
 ?>
